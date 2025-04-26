f85bafc87490e61a681de1a0b04bb6e02c790e60
Loukas Bitsakis <117123896+loukab@users.noreply.github.com>
2025-04-26 13:07:34 +0000

    First commit make the nine spaces

--- src/test/java/com/example/project/TicTacToeTests.java
+++ src/test/java/com/example/project/TicTacToeTests.java
@@ -17,25 +17,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class CalculatorTests {
+class TicTacToeTests {
 
 	@Test
 	@DisplayName("1 + 1 = 2")
-	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+	void GameStartBuildNineBoard() {
+		TicTacToe game = new TicTacToeGame();
+		assertEquals(9, game.avaliableFieldsNumber());
 	}
 
-	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@CsvSource({
-			"0,    1,   1",
-			"1,    2,   3",
-			"49,  51, 100",
-			"1,  100, 101"
-	})
-	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
-	}
+
+
+
+
+
+
+	
 }
